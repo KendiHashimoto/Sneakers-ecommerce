a9dfc5b29b312dc57fb7c5710cce290ff8b69e9e
KendiHashimoto <kendi.campos@hotmail.com>
2022-12-15 18:17:31 -0300

    JS configs 📦

--- src/main.js
+++ src/main.js
@@ -1,2 +1,37 @@
-import './tailwind.css'
+import "./tailwind.css"
 
+const burgerMenu = document.querySelector(".hamburger-menu")
+const cart = document.querySelector(".inactive-cart-icon")
+const cartModal = document.querySelector(".cart-modal")
+
+const removeSneakerIcon = document.querySelector(".fa-minus")
+const sneakersNumber = document.querySelector(".sneakers-number")
+const addSneakerIcon = document.querySelector(".fa-plus")
+
+const addToCart = document.querySelector(".addToCart-button")
+
+// const cartSneakers = (symbol) => {
+//   if (symbol.classList.contains("addSneakerIcon")) {
+//     console.log('josepe')
+//   }
+//     sneakersNumber.innerText = +sneakersNumber.innerText + number
+// }
+
+// * EVENTS
+cart.addEventListener("click", () => {})
+
+addSneakerIcon.addEventListener("click", (e) => {
+  if(e.target.classList.contains(".fa-plus")) {
+    console.log('josepe')
+  }
+  // cartSneakers(e.target)
+  // cartSneakers(1)
+})
+
+// removeSneakerIcon.addEventListener("click", (e) => {
+//   cartSneakers(-1)
+// })
+
+addToCart.addEventListener("click", () => {})
+
+burgerMenu.addEventListener("click", () => {})
